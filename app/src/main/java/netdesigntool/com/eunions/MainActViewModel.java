c193184efc57ff09e0c16b3ea0f234a044014f6e
Lenovo <vicpls@mail.ru>
2021-03-17 19:02:16 +0300

    Asynchronous read countries table and fetch liveData for MainAct.

--- app/src/main/java/netdesigntool/com/eunions/MainActViewModel.java
+++ app/src/main/java/netdesigntool/com/eunions/MainActViewModel.java
@@ -1,32 +1,44 @@
 package netdesigntool.com.eunions;
 
 import android.app.Application;
+import android.util.Log;
 
 import androidx.annotation.NonNull;
 import androidx.lifecycle.AndroidViewModel;
 import androidx.lifecycle.MutableLiveData;
 
 import java.util.ArrayList;
+import java.util.concurrent.atomic.AtomicBoolean;
 
 import static netdesigntool.com.eunions.DataRepository.getDataRepository;
+import static netdesigntool.com.eunions.Util.LTAG;
 
 public class MainActViewModel extends AndroidViewModel {
 
-    MutableLiveData<Country[]> ldSchAndEu;
-    MutableLiveData<Country[]> ldSchen;
-    MutableLiveData<Country[]> ldEu;
+    MutableLiveData<Country[]> ldSchAndEu = new MutableLiveData<>();
+    MutableLiveData<Country[]> ldSchen = new MutableLiveData<>();
+    MutableLiveData<Country[]> ldEu = new MutableLiveData<>();
+
+    private final AtomicBoolean isFetched = new AtomicBoolean(false);
 
     public MainActViewModel(@NonNull Application application) {
         super(application);
     }
 
+    private void startFetchLdCountries(){
+
+        Log.d(LTAG, "isFetched= "+ isFetched);
+
+        if ( isFetched.compareAndSet(false, true)){
+            new Thread(this::fetchLdCountriesByType).start();
+        }
+    }
+
     private void fetchLdCountriesByType(){
 
-        Country[] countries = getDataRepository().loadCountries(getApplication().getApplicationContext());   // todo: делать Асинхронно!!
+        Log.d(LTAG, "Start fetchLdCountriesByType()");
 
-        ldSchAndEu = new MutableLiveData<>();
-        ldSchen = new MutableLiveData<>();
-        ldEu = new MutableLiveData<>();
+        Country[] countries = getDataRepository().loadCountries(getApplication().getApplicationContext());
 
         ArrayList<Country> eu = new ArrayList<>(10);
         ArrayList<Country> schen = new ArrayList<>(10);
@@ -40,30 +52,32 @@ public class MainActViewModel extends AndroidViewModel {
             if (c.isSchen()) schen.add(c);            // Shengen only
         }
 
-        ldSchAndEu.setValue(schAndEu.toArray(new Country[0]));
-        ldSchen.setValue(schen.toArray(new Country[0]));
-        ldEu.setValue(eu.toArray(new Country[0]));
+        ldSchAndEu.postValue(schAndEu.toArray(new Country[0]));
+        ldSchen.postValue(schen.toArray(new Country[0]));
+        ldEu.postValue(eu.toArray(new Country[0]));
+
+        Log.d(LTAG, "Finish fetchLdCountriesByType()");
     }
 
 
 
     public MutableLiveData<Country[]> getSchAndEu (){
 
-        if (ldSchAndEu ==null) fetchLdCountriesByType();
+        startFetchLdCountries(); //fetchLdCountriesByType();
 
         return ldSchAndEu;
     }
 
     public MutableLiveData<Country[]> getSchen(){
 
-        if (ldSchen ==null) fetchLdCountriesByType();
+        startFetchLdCountries(); //fetchLdCountriesByType();
 
         return ldSchen;
     }
 
     public MutableLiveData<Country[]> getEu(){
 
-        if (ldEu ==null) fetchLdCountriesByType();
+        startFetchLdCountries();  //fetchLdCountriesByType();
 
         return ldEu;
     }
