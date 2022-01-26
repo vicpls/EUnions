005149e3b3d418445a2d9cd874808c5cc5059d1b
Acer win J <vicpls@mail.ru>
2022-01-26 16:36:05 +0300

    Start refactor for using Hilt in FirebaseDataProvider.

---
+++ app/src/main/java/netdesigntool/com/eunions/firebase/FirebaseAttributeForApp.kt
@@ -0,0 +1,7 @@
+package netdesigntool.com.eunions.firebase
+
+@Singleton
+class FirebaseAttributeForApp @Inject constructor() {
+    private const val URL_REF ="https://fir-a0980.firebaseio.com/"  // URL of firebase
+    private const val BASE_NAME = "Country"
+}
