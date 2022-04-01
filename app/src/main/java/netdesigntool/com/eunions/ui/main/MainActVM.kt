ce2bfc27b73f0ee829be3c5875066a1906f22c01
Acer win J <vicpls@mail.ru>
2022-04-01 18:36:52 +0300

    #EU-17 day upd.

---
+++ app/src/main/java/netdesigntool/com/eunions/ui/main/MainActVM.kt
@@ -0,0 +1,67 @@
+package netdesigntool.com.eunions.ui.main
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import netdesigntool.com.eunions.Country
+import netdesigntool.com.eunions.local_db.AppDatabase
+import java.util.concurrent.atomic.AtomicBoolean
+import javax.inject.Inject
+
+@HiltViewModel
+class MainActVM @Inject constructor(val appDb : AppDatabase) : ViewModel(){
+
+    val ldSchAndEu : LiveData<List<Country>>
+        get() = _ldSchAndEu
+    private val _ldSchAndEu = MutableLiveData<List<Country>>()
+        get() = field.also{startFetchLdCountries()}
+
+    val ldSchen : LiveData<List<Country>>
+        get() = _ldSchen
+    private val _ldSchen = MutableLiveData<List<Country>>()
+        get() = field.also{startFetchLdCountries()}
+
+    val ldEu  : LiveData<List<Country>>
+        get() = _ldEu
+    private val _ldEu = MutableLiveData<List<Country>>()
+        get() = field.also{startFetchLdCountries()}
+
+    private val isFetched = AtomicBoolean(false)
+
+
+    private fun startFetchLdCountries(){
+
+        if (isFetched.compareAndSet(false, true)) return
+
+        CoroutineScope(Dispatchers.IO).launch {
+            fetchLdCountries()
+        }
+    }
+
+
+    private suspend fun fetchLdCountries(){
+
+        val eu = mutableListOf<Country>()       // EU only
+        val schen = mutableListOf<Country>()    // Shengen only
+        val schAndEu = mutableListOf<Country>() // Shengen + EU
+
+        appDb.countriesDao()
+                .getMemberCountries()
+                .forEach { country ->
+                    when {
+                        (country.isEU && country.isSchen) -> schAndEu.add(country)
+                        (country.isEU) -> eu.add(country)
+                        (country.isSchen) -> schen.add(country)
+                    }
+                }
+
+        _ldEu.postValue(eu)
+        _ldSchAndEu.postValue(schAndEu)
+        _ldSchen.postValue(schen)
+    }
+
+}
