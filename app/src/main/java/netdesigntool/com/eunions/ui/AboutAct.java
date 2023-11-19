4a99948009c2b7c456f7f3c4b9ba5fb7516a3a4e
VictorE <victore.adev@gmail.com>
2023-11-19 18:36:18 +0300

    fix: About act

--- app/src/main/java/netdesigntool/com/eunions/ui/AboutAct.java
+++ app/src/main/java/netdesigntool/com/eunions/ui/AboutAct.java
@@ -22,6 +22,13 @@ public class AboutAct {
         License apacheLic2 = new ApacheSoftwareLicense20();
 
         notices.addNotice(
+                new Notice("Privacy Policy",
+                        "https://raw.githubusercontent.com/vicpls/PPolicy/main/PPolicy-4gp.txt",
+                        null,
+                        new PrivacyPolicy()
+                ));
+
+        notices.addNotice(
                 new Notice("AboutActivity"
                         , "https://github.com/biagiopietro/AboutActivity"
                         , "Copyright 2018 biagiopietro"
@@ -51,20 +58,38 @@ public class AboutAct {
         new AboutActivityBuilder.Builder(act)
                 .showAppVersion(true, "Version: ", BuildConfig.VERSION_NAME)
                 .setAppLogo(R.mipmap.eur_union_192)
-                .setCompanyLogo(R.drawable.logo_mc_soft)
+                .setCompanyLogo(R.mipmap.eur_union_192)
                 .showRateApp(true, act.getPackageName(), "Please, rate my app")
-                .showLicense(true, notices, "Credit", "Credit", "Close")
-                //.showLicense(true, credit, "Credit", "Credit", "Close")
+                .showLicense(true, notices, "Credit & Privacy Policy", "Credit & Privacy Policy", "Close")
+                //.showLicense(true, notices2, "Credit & Privacy Policy", "Credit & Privacy Policy", "Close")
                 .showGeneral(true
-                        , "EUnion"  //getString(R.string.app_name)
+                        , "EUnion"
                         , act.getPackageName()
                         , act.getString(R.string.developed)
                         , act.getString(R.string.thanks)
                         , act.getString(R.string.share))
+
                 .showAboutActivity();
     }
 
-    final private class Redpixart extends License {
+
+    static final private class PrivacyPolicy extends License {
+        @Override
+        public String getName() {return "PrivacyPolicy";}
+        @Override
+        public String readSummaryTextFromResources(Context context) {return "";}
+        @Override
+        public String readFullTextFromResources(Context context) {return "";}
+        @Override
+        public String getVersion() {return "1.0";}
+        @Override
+        public String getUrl() {
+            return "https://github.com/vicpls/etc/blob/main/PPolicy-4gp.txt";
+        }
+    }
+
+
+    static final private class Redpixart extends License {
 
         @Override
         public String getName() {
