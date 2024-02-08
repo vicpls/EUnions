09d1569ee6c64a56b0d2022e4bb4ee937527e542
VictorE <victore.adev@gmail.com>
2024-02-08 19:36:46 +0300

    build: now use transitive R Class

--- app/src/androidTest/java/netdesigntool/com/eunions/ui/screens/AboutActSc.kt
+++ app/src/androidTest/java/netdesigntool/com/eunions/ui/screens/AboutActSc.kt
@@ -6,18 +6,18 @@ import io.github.kakaocup.kakao.image.KImageView
 import io.github.kakaocup.kakao.rating.KRatingBar
 import io.github.kakaocup.kakao.text.KButton
 import io.github.kakaocup.kakao.text.KTextView
-import netdesigntool.com.eunions.R
 import netdesigntool.com.eunions.ui.AboutAct
+import com.mcsoft.aboutactivity.R.id as aboutActRId
 
 object AboutActSc : KScreen<AboutActSc>() {
     override val layoutId: Int?
         get() = null
-    override val viewClass: Class<*>?
+    override val viewClass: Class<*>
         get() = AboutAct::class.java
 
-    val appLogo = KImageView { withId(R.id.imgLogoApp)}
-    val shareFab = KButton { withId(R.id.fabShare)}
-    val version = KTextView { withId(R.id.lblVersion)}
-    val cardLicense = KView { withId(R.id.cardLicense)}
-    val ratingBar = KRatingBar { withId(R.id.ratingBar)}
+    val appLogo = KImageView { withId(aboutActRId.imgLogoApp)}
+    val shareFab = KButton { withId(aboutActRId.fabShare)}
+    val version = KTextView { withId(aboutActRId.lblVersion)}
+    val cardLicense = KView { withId(aboutActRId.cardLicense)}
+    val ratingBar = KRatingBar { withId(aboutActRId.ratingBar)}
 }
